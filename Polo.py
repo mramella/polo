c5b02a909dc03dae239536b8fcbc69fa25ea0ece
Matthew Ramella <matthewramella@yahoo.com>
2017-10-14 07:43:23 -0500

    Bug fix and removed call to DynamDB
    Fixed bug when calculating total total balance.  Needed to account for USDT using the 'available' attribute from the payload, as opposed to the 'btcValue' attribute.  With this fix, the returned total balance now matches the Polo UI down to the penny.

--- Polo.py
+++ Polo.py
@@ -1,79 +1,76 @@
 def lambda_handler(event, context):
-    p = poloniex(os.environ['KEY'], os.environ['SECRET'])
 
+    p = poloniex(os.environ['KEY'], os.environ['SECRET'])
+    
     poloBalance = p.returnBalance()
 
-    dynamodb_client = boto3.client('dynamodb')
-
-    item = {'datetime': {'S': str(datetime.datetime.now())}, 'balance': {'S': poloBalance}}
-
-    dynamodb_client.put_item(TableName='polo', Item=item)
-
-    return json.loads(
-        '{"version":"1.0","response":{"outputSpeech":{"type":"PlainText","text":"Your polo balance is ' + str(
-            poloBalance) + ' USD. Nice! "},"shouldEndSession":true}}')
-
+    return json.loads('{"version":"1.0","response":{"outputSpeech":{"type":"PlainText","text":"Your polo balance is ' + str(poloBalance) + ' USD."},"shouldEndSession":true}}')
 
 import urllib
 import json
 import hmac
 import hashlib
-import time
+import time 
 import os
 import boto3
 import datetime
 
-
 class poloniex:
+
     def __init__(self, APIKey, Secret):
         self.APIKey = APIKey
         self.Secret = Secret
-
+ 
     def api_query(self, command, req={}):
-
-        req['command'] = command
-        req['nonce'] = int(time.time() * 1000)
-
-        post_data = urllib.parse.urlencode(req)
-        post_data = post_data.encode('utf-8')  # data should be bytes
-
-        sign = hmac.new(bytes(self.Secret, 'latin-1'), post_data, hashlib.sha512).hexdigest()
-
-        headers = {
-            'Sign': sign,
-            'Key': self.APIKey
-        }
-
-        ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/tradingApi', post_data, headers))
-
-        return json.loads(ret.read())
+            
+            req['command'] = command
+            req['nonce'] = int(time.time()*1000)
+            
+            post_data = urllib.parse.urlencode(req)
+            post_data = post_data.encode('utf-8') # data should be bytes
+ 
+            sign = hmac.new(bytes(self.Secret, 'latin-1'), post_data, hashlib.sha512).hexdigest()
+            
+            headers = {
+                'Sign': sign,
+                'Key': self.APIKey
+            }
+ 
+            ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/tradingApi', post_data, headers))
+            
+            return json.loads(ret.read())
 
     def get_bitcoin_price_polo(self):
-
-        ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/public?command=returnTicker'))
-
-        payload = json.loads(ret.read())
-
-        bitcoinPrice = payload['USDT_BTC']['last']
-
-        bitcoinPriceFloat = float(bitcoinPrice)
-
-        return bitcoinPriceFloat
+        
+            ret = urllib.request.urlopen(urllib.request.Request('https://poloniex.com/public?command=returnTicker'))
+            
+            payload = json.loads(ret.read())
+            
+            bitcoinPrice = payload['USDT_BTC']['last']
+            
+            bitcoinPriceFloat = float(bitcoinPrice)
+            
+            return bitcoinPriceFloat
 
     def returnBalance(self):
-
+        
         bitcoinPrice = self.get_bitcoin_price_polo()
-
+        
         balances = self.api_query('returnCompleteBalances')
 
         totalBtcBalance = 0
-
+        totalUsdtBalance = 0
+        
         for coin in balances:
             balancePerCoin = balances[coin]
-            btcValue = float(balancePerCoin['btcValue'])
-            if btcValue > 0:
-                totalBtcBalance = totalBtcBalance + btcValue
+            if coin == 'USDT':
+                totalUsdtBalance = float(balancePerCoin['available'])
+            else:
+                btcValue = float(balancePerCoin['btcValue'])
+                if btcValue > 0:
+                    totalBtcBalance = totalBtcBalance + btcValue
 
-        totalValueInUSD = round((totalBtcBalance * bitcoinPrice), 2)
-
-        return str(totalValueInUSD)
+        totalValueInUSD = (totalBtcBalance * bitcoinPrice) + totalUsdtBalance
+        
+        return str(round(totalValueInUSD, 2))
+ 
